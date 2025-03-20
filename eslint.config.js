cc9d6f1df1a26a586c2b1fc6fa9b9a270b19e7ba
wangzhicheng <514046413@qq.com>
2025-03-20 19:03:57 +0800

    feat(error): add ErrorVitals

--- eslint.config.js
+++ eslint.config.js
@@ -7,5 +7,11 @@ export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  }
 ]
